7183f9208d1d37eb4a4635602952f09db9b7a923
Rreguero <39240276+Rreguero@users.noreply.github.com>
2019-10-24 09:24:23 +0200

    asdr

--- ErronkaBat/src/Eredua/Konexioa.java
+++ ErronkaBat/src/Eredua/Konexioa.java
@@ -1,5 +1,58 @@
 package Eredua;
+import java.sql.*;
+
 
 public class Konexioa {
+	
+	private String usuario = "root";
+	private String pwd = "";
+	private static String bd = "ERRONKA1";
+	private String url = "jdbc:mysql://localhost/" + bd;
+	private Connection konx = null;
+	
+	{
+
+		try {
+			Class.forName("com.mysql.jdbc.Connection");
+			konx = (Connection) DriverManager.getConnection(url, usuario, pwd);
+			if (konx != null) {
+				System.out.println("Ondo joan da konexioa, " + url + " . . . Konektatua");
+			}
+		} catch (SQLException | java.lang.ClassNotFoundException ex) {
+			System.out.println(ex.getMessage());
+		}
+
+	}
+
+	public ResultSet getQuery(String _query) {
+		Statement state = null;
+		ResultSet resultado = null;
+		try {
+			state = (Statement) konx.createStatement();
+			resultado = state.executeQuery(_query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return resultado;
+	}
+
+	public void setQuery(String _query) {
+
+		Statement state = null;
+
+		try {
+			state = (Statement) konx.createStatement();
+			state.executeUpdate(_query);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void itxi() throws SQLException{
+		konx.close();
+	}
+	
+	
 
 }
